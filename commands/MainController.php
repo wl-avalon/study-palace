9bd0e8fd25c752dd2339edb532e6ea812f08a01e
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-01-29 21:52:03 +0800

    提交代码

--- commands/MainController.php
+++ commands/MainController.php
@@ -8,36 +8,103 @@
 
 namespace app\commands;
 use app\components\Format;
+use app\components\MockData;
 use app\library\Request;
 use yii\console\Controller;
 
 class MainController extends Controller
 {
     public function actionIndex($startUrl){
-        $requestUrl = $startUrl;
-        do{
-            $this->curlOne($requestUrl);
-            exit;
-        }while(true);
+        set_time_limit(0);
+//        $requestUrl = $startUrl;
+//        $record = $this->curlOne($requestUrl);
+        $record = MockData::getTestRecord();
+        $record = json_decode($record, true);
+        foreach($record as $xueDuanIndex => $xueKeData){
+            $xueKeList = $xueKeData['data'];
+            foreach($xueKeList as $xueKeIndex => $banBenData){
+                $banBenList = $banBenData['data'];
+                foreach($banBenList as $banBenIndex => $moduleData){
+                    $moduleList = $moduleData['data'];
+                    foreach($moduleList as $moduleIndex => $module){
+                        $this->getQuestion($xueDuanIndex, $xueKeIndex, $banBenIndex, $moduleIndex, $module['data']);
+                    }
+                }
+            }
+        }
     }
 
     public function curlOne($url, $nowLevel = 1){
-        sleep(1);
-        $response = Request::curl($url);
-        $levelList = Format::getLevelMap($response, $nowLevel);
-        if($nowLevel == 4){
-            return $levelList;
+        usleep(100000);
+        $response   = Request::curl($url);
+
+        if($nowLevel == 5){
+            $tiXingMap  = Format::getTiXingMap($response);
+            $nanDuMap   = Format::getNanDuMap($response);
+            $nodeList   = Format::getNodeList($response);
+            return [
+                'nanDuMap'  => $nanDuMap,
+                'tiXingMap' => $tiXingMap,
+                'nodeList'  => $nodeList,
+            ];
+        }else{
+            $levelList  = Format::getLevelMap($response, $nowLevel);
         }
         $record = [];
         foreach($levelList as $level => $levelValue){
+            if($level == 5){
+                return $record;
+            }
+            if($level == 11){
+                return $record;
+            }
             if($nowLevel == 1){
                 $urlTemp = rtrim($url, '/') . "/{$level}";
             }else{
                 $urlTemp = "{$url},{$level}";
             }
-            echo "{$level}-----{$levelValue}----{$urlTemp}\n";
-            $record[$level] = $this->curlOne($urlTemp, $nowLevel + 1);
+            echo "{$level},{$levelValue}\n";
+            $record[$level] = [
+                'key'   => $level,
+                'value' => $levelValue,
+                'data'  => $this->curlOne($urlTemp, $nowLevel + 1),
+            ];
         }
         return $record;
+    }
+
+    public function getQuestion($xueDuan, $xueKe, $banBen, $moduleIndex, $moduleData){
+        $url = "http://www.91taoke.com/Juanzi/ajaxlist?id={$xueDuan},{$xueKe},{$banBen},{$moduleIndex}&zjid=0&tixing=0&nandu=0&leixing=0";
+//        $url = "http://www.91taoke.com/Juanzi/ajaxlist?id=3,9,27,1000001&zjid=0&tixing=114&nandu=0&search=&leixing=0&xuekename=语文";
+//        $url = "http://www.91taoke.com/Juanzi/ajaxlist?id=3%2C11%2C37%2C1000037&zjid=0&tixing=0&nandu=0&leixing=0&xuekename=%E6%95%B0%E5%AD%A6";
+        $response       = Request::curl($url);
+        $questList  = Format::getQuestInfo($response);
+
+        $questionListRes = [];
+        foreach($questList as $questInfo){
+            $question   = Format::formatQuestionContent($questInfo['questionContent']);
+            $result     = Format::formatResultContent($questInfo['resultContent']);
+
+            $questionRemark = $question['questionRemark'];
+            $questionList   = $question['questionList'];
+            $resultRemark   = $result['resultRemark'];
+            $resultList     = $result['resultList'];
+
+            $list = [];
+            foreach($questionList as $key => $questionText){
+                $item = [
+                    'question'  => $questionText,
+                    'result'    => $resultList[$key] ?? "",
+                ];
+                $list[] = $item;
+            }
+            $questionListRes[] = [
+                'questionRemark'    => $questionRemark,
+                'resultRemark'      => $resultRemark,
+                'questList'         => $list,
+            ];
+        }
+        echo json_encode($questionListRes);exit;
+        return $questionListRes;
     }
 }
--- components/Format.php
+++ components/Format.php
@@ -7,10 +7,14 @@
  */
 namespace app\components;
 
-use app\models\beans\TopicBean;
-
 class Format
 {
+    /**
+     * 获取页面tab映射
+     * @param $response
+     * @param $chooseLevel
+     * @return mixed
+     */
     public static function getLevelMap($response, $chooseLevel){
         $matchStr = [];
         preg_match_all("/\B<a[^>]*?aclick\(([1234]),([0-9]*)\)[^>]*?>([^<]*?)<\/a>\B/", $response, $matchStr);
@@ -21,6 +25,184 @@ class Format
         foreach($tabLevelList as $index => $level){
             $tabLevelMap[$level][$keyList[$index]] = $valueList[$index];
         }
+        if($chooseLevel == 5){
+            var_dump($tabLevelMap);exit;
+        }
         return $tabLevelMap[$chooseLevel];
+    }
+
+    /**
+     * 获取题型映射
+     * @param $response
+     * @return array
+     */
+    public static function getTiXingMap($response){
+        $matchStr = [];
+        preg_match_all("/\B<a[^>]*?tixingclick\(([0-9]*)\)[^>]*?>([^<]*?)<\/a>\B/", $response, $matchStr);
+        $tiXingIndexList    = $matchStr[1];
+        $tiXingValueList    = $matchStr[2];
+
+        $tiXingMap = [];
+        foreach($tiXingIndexList as $tiXingIndex => $tiXingValue){
+            $tiXingMap[$tiXingValue] = $tiXingValueList[$tiXingIndex];
+        }
+        return $tiXingMap;
+    }
+
+    /**
+     * 获取难度映射
+     * @param $response
+     * @return array
+     */
+    public static function getNanDuMap($response){
+        $matchStr = [];
+        preg_match_all("/\B<a[^>]*?nanduclick\((.*?)\)[^>]*?>([^<]*?)<\/a>\B/", $response, $matchStr);
+        $nanDuIndexList    = $matchStr[1];
+        $nanDuValueList    = $matchStr[2];
+
+        $nanDuMap = [];
+        foreach($nanDuIndexList as $nanDuIndex => $nanDuValue){
+            $nanDuMap[trim($nanDuValue, "'")] = $nanDuValueList[$nanDuIndex];
+        }
+        return $nanDuMap;
+    }
+
+    public static function getNodeList($response){
+        $matchStr   = [];
+        preg_match_all("/\bvar zNodes = (\[[^\]]*?\])\B/", $response, $matchStr);
+        $nodeList   = $matchStr[1][0];
+
+        $nodeList   = str_replace("'", '"', $nodeList);
+        $nodeList   = str_replace("id", '"id"', $nodeList);
+        $nodeList   = str_replace("pId", '"pId"', $nodeList);
+        $nodeList   = str_replace("name", '"name"', $nodeList);
+        $nodeList   = json_decode($nodeList, true);
+        return $nodeList;
+    }
+
+    public static function getQuestInfo($response){
+        $matchStr   = [];
+        preg_match_all('/\B<div class="questInfo[^>]*?>([\s\S]*?)<p class="questInfoTitle[^>]*?>/', $response, $matchStr);
+        $questionAndResult = $matchStr[1] ?? [];
+
+        $questInfoList = [];
+        foreach($questionAndResult as $item){
+            $xhrQuestion    = [];
+            preg_match_all('/\B<div class="questInfo xhr_questInfo">([\s\S]*?)<\/div>\B/', $item, $xhrQuestion);
+            $question       = [];
+            preg_match_all('/\B<a href="javascript:t_toggle\(([0-9]*?)\);">[\s\S]*?(<p>[\s\S]*?)<\/a>\B/', $item, $question);
+            $questionKeyList    = $question[1];
+            $questionValueList  = $question[2];
+            $questionList       = [];
+            foreach($questionKeyList as $index => $key){
+                $questionList[] = [
+                    'key'   => $key,
+                    'value' => $questionValueList[$index],
+                ];
+            }
+            $questionContent    = [
+                'questionRemark'    => $xhrQuestion[1],
+                'questionList'      => $questionList,
+            ];
+
+            $result             = [];
+            preg_match_all('/\B<div class="result" id="result_([0-9]*?)">[\s\S]*?(<table>[\s\S]*?<\/table>)\B/', $item, $result);
+            $resultKeyList      = $result[1];
+            $resultValueList    = $result[2];
+            $resultList         = [];
+
+            foreach($resultKeyList as $index => $key){
+                $resultList[] = [
+                    'key'   => $key,
+                    'value' => $resultValueList[$index],
+                ];
+            }
+            $resultContent      = [
+                'resultRemark'  => '',
+                'resultList'    => $resultList,
+            ];
+
+            $questInfoList[]    = [
+                'questionContent'   => $questionContent,
+                'resultContent'     => $resultContent,
+            ];
+        }
+        return $questInfoList;
+    }
+
+    public static function formatQuestionContent($questionContent){
+        $remarkRes          = "";
+        $questionRemark     = $questionContent['questionRemark'];
+        foreach($questionRemark as $remarkItem){
+            $remarkRes      .= self::formatWhiteTag($remarkItem);
+        }
+        $remarkRes          = trim($remarkRes);
+
+        $questionListRes    = [];
+        $questionList       = $questionContent['questionList'];
+        foreach($questionList as $questionItem){
+            $key    = $questionItem['key'];
+            $value  = self::formatWhiteTag($questionItem['value']);
+            $questionListRes[$key] = trim($value);
+        }
+        return [
+            'questionRemark'    => $remarkRes,
+            'questionList'      => $questionListRes,
+        ];
+    }
+
+    public static function formatResultContent($resultContent){
+        $remarkRes      = "";
+
+        $resultListRes  =   [];
+        $resultList     = $resultContent['resultList'];
+        foreach($resultList as $result){
+            $key                    = $result['key'];
+            $value                  = $result['value'];
+            $result                 = trim($value);
+            $temp                   = str_replace('&nbsp;', "", $result);
+            $resultItem             = self::formatResultItem($temp);
+            $resultListRes[$key]    = $resultItem;
+        }
+        return [
+            'resultRemark'  => $remarkRes,
+            'resultList'    => $resultListRes,
+        ];
+    }
+
+    public static function formatResultItem($item){
+        $matchArr = [];
+        preg_match_all('/<td[^>]*?>([\s\S]*?)<\/td>/', $item, $matchArr);
+        $resultData = $matchArr[1];
+
+        $resultRes = [];
+        $lastKey = "";
+        foreach($resultData as $index => $resultItem){
+            if($index % 2 != 0){
+                $resultRes[$lastKey] = trim(self::formatWhiteTag($resultItem));
+            }
+            switch($resultItem){
+                case "【答案】":{$lastKey = "answer";break;}
+                case "【解析】":{$lastKey = "analysis";break;}
+                case "【知识点】":{$lastKey = "knowledge_point";break;}
+                case "【题点】":{$lastKey = "question_poin";break;}
+                case "【难易度】":{$lastKey = "difficulty";break;}
+                case "【日期】":{$lastKey = "date";break;}
+            }
+        }
+        return $resultRes;
+    }
+
+    public static function formatWhiteTag($content){
+        $question       = trim($content);
+        $temp           = preg_replace('/[^\S]*?<p[^>]*?>[^\S]*/', "", $question);
+        $temp           = preg_replace('/[^\S]*?<li[^>]*?>[^\S]*/', "", $temp);
+        $temp           = trim($temp);
+        $temp           = preg_replace('/[^\S]*?<\/li[^>]*?>[^\S]*/', "\n", $temp);
+        $temp           = preg_replace('/[^\S]*?<\/p[^>]*?>[^\S]*/', "\n", $temp);
+        $temp           = preg_replace('/<[\/]*?if[^>]*?>/', "", $temp);
+        $temp           = str_replace("<ol style=\"clear: both\">", "", $temp);
+        $temp           = str_replace("</ol>", "", $temp);
+        return $temp;
     }
 }
