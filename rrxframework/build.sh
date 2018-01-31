cd413799dd289ddcf8af4909a15fbec41afe9531
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-01-31 16:43:09 +0800

    删除框架代码

--- rrxframework/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-MODULE_NAME="rrxframework"
-rm -rf output
-mkdir -p output/application/$MODULE_NAME
-cp -r base util ext service components output/application/$MODULE_NAME
-cd output
-tar cvzf $MODULE_NAME.tar.gz application
-rm -rf application
--- rrxframework/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#基于 fis3，fis3安装：npm install fis3 -g
-fis3 release -d ../application -w
