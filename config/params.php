8e76dda003b0d5965776bd4ebde6960e7a555735
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-01-31 17:57:54 +0800

    提交代码

--- config/params.php
+++ config/params.php
@@ -2,4 +2,11 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'idgent' => [
+        'domain'    => 'http://127.0.0.1',
+        'apis' => [
+            'nextId'    => '/nextId',      //取一个ID
+            'batch'     => '/batch',        //取多个ID
+        ],
+    ],
 ];
