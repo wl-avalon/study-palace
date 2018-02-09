34207b4bbce4051523fc5d79cf6713c3243d31ac
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-02-09 18:17:34 +0800

    提交代码

--- commands/MainController.php
+++ commands/MainController.php
@@ -11,6 +11,7 @@ use app\components\MockData;
 use app\components\SPLog;
 use app\constants\RedisKey;
 use app\library\Proxy;
+use app\services\daemon\process\CreateQuestionDetailService;
 use app\services\daemon\spider\CreateQuestionService;
 use app\services\daemon\spider\GetProxyIPListService;
 use rrxframework\util\RedisUtil;
@@ -54,6 +55,11 @@ class MainController extends Controller
             }
             sleep(5);
         }
+    }
+
+    public function actionCreateQuestionDetail($processName){
+        set_time_limit(0);
+        CreateQuestionDetailService::execute($processName);
     }
 }
 
